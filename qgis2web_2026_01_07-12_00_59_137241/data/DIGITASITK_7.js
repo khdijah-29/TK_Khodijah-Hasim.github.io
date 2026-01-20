f61500d58db9aac9a22cd5219f2a69a827ae36b5
Khodijah-H <khodijahhasimmurad@gmail.com>
2026-01-20 11:51:26 +0700

    Add files via upload

---
+++ qgis2web_2026_01_07-12_00_59_137241/data/DIGITASITK_7.js
@@ -0,0 +1,18 @@
+var json_DIGITASITK_7 = {
+"type": "FeatureCollection",
+"name": "DIGITASITK_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"xy_coordinate_resolution": 1e-06,
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama": "TK Satu Atap SDN Bandungrejosari I\n", "Alamat": "SDN, Jl. Raya Kepuh No.179, Bandungrejosari, Sukun, Malang City, East Java 65148", "Telepon": "0341804443" }, "geometry": { "type": "Point", "coordinates": [ 112.618948, -8.007769 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama": "TK Aisyiyah 23 Malang", "Alamat": "Jl. Raya Kepuh No.lX 2, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "0341805268" }, "geometry": { "type": "Point", "coordinates": [ 112.620673, -8.013542 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama": "KB - TK Al Falah Malang\n", "Alamat": "Jl. Klayatan Gg. I No.18, RT.6/RW.1, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "0341369487" }, "geometry": { "type": "Point", "coordinates": [ 112.61596, -7.999748 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nama": "TK DHARMA WANITA III KLAYATAN", "Alamat": "Jl. Klayatan 1 No.17, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 41568", "Telepon": "0" }, "geometry": { "type": "Point", "coordinates": [ 112.614783, -7.99869 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nama": "TK Aisyiyah Bustanul Athfal 30\n", "Alamat": "Jl. Kemantren 3 Jl. Abdul Jalil No.65148, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "0341834860" }, "geometry": { "type": "Point", "coordinates": [ 112.609746, -8.000628 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nama": "TK 'Aisyiyah ABA 31 Malang", "Alamat": "Jl. Klayatan Gg. I Jl. Klayatan No.39, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "03419323121" }, "geometry": { "type": "Point", "coordinates": [ 112.615928, -7.998609 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Nama": "Permata Iman 3\n", "Alamat": "Jl. Keben II Blok 2 A No.1, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "0341805155" }, "geometry": { "type": "Point", "coordinates": [ 112.619437, -8.006318 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Nama": "Pendidikan Anak Usia Dini PAUD Play Group - TK Plus Qurrota A'yun Ii", "Alamat": "Jl. Klayatan II No.7, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "082132880747" }, "geometry": { "type": "Point", "coordinates": [ 112.613976, -8.003529 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Nama": "TK Dharma wanita 2\n", "Alamat": "Jl. Janti Sel. No.69, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "0" }, "geometry": { "type": "Point", "coordinates": [ 112.625735, -8.006649 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Nama": "World Kids Center", "Alamat": "Jl. Klayatan II No.5, Bandungrejosari, Kec. Sukun, Kota Malang, Jawa Timur 65148", "Telepon": "081334320881" }, "geometry": { "type": "Point", "coordinates": [ 112.614325, -8.002182 ] } }
+]
+}
